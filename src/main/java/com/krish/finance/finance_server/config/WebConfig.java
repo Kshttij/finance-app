b8ea927799fb4a05ef0f03b97a11d9dcfafdd8fd
Kshttij <kshitijsingh2k4@gmail.com>
2025-08-24 04:15:28 +0530

    corrected apis

--- src/main/java/com/krish/finance/finance_server/config/WebConfig.java
+++ src/main/java/com/krish/finance/finance_server/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // allow all endpoints
-                        .allowedOrigins("https://finance-app-seven-neon.vercel.app/") // your Vite frontend
+                        .allowedOrigins("https://finance-app-seven-neon.vercel.app") // your Vite frontend
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
