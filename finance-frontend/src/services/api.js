df3811e0611845f8b5d1cc85a27d653b7bd219c5
Kshttij <kshitijsingh2k4@gmail.com>
2025-08-24 04:06:09 +0530

    resolved ig

--- finance-frontend/src/services/api.js
+++ finance-frontend/src/services/api.js
@@ -3,7 +3,7 @@ const API_URL = "https://finance-server-cs6w.onrender.com";
 
 // -------------------- Auth APIs --------------------
 export async function signup(userData) {
-  const response = await fetch(`${API_URL}/signup`, {
+  const response = await fetch(`${API_URL}/api/signup`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(userData),
@@ -12,7 +12,7 @@ export async function signup(userData) {
 }
 
 export async function login(userData) {
-  const response = await fetch(`${API_URL}/login`, {
+  const response = await fetch(`${API_URL}/api/login`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(userData),
